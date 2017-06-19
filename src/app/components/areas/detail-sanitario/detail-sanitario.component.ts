548f186847b024adafe66e36b1647d73f879c49c
eyegio <g.segagni@gmail.com>
2017-06-19 12:04:37 -0400

    added style and animation to detail components

--- src/app/components/areas/detail-sanitario/detail-sanitario.component.ts
+++ src/app/components/areas/detail-sanitario/detail-sanitario.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-detail-sanitario',
   templateUrl: './detail-sanitario.component.html',
-  styleUrls: ['./detail-sanitario.component.sass']
+  styleUrls: ['./detail-sanitario.component.sass'],
+  animations: [
+    trigger('rotateAnimation', [
+      state('inactive', style({
+        transform: 'rotate(0deg)'
+      })),
+      state('active', style({
+        transform: 'rotate(360deg)'
+      })),
+      transition('inactive <=> active', animate('400ms ease'))
+    ])
+  ]
 })
 export class DetailSanitarioComponent implements OnInit {
+  state: string = 'inactive';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  animateMe(){
+    this.state = (this.state === "inactive" ? "active" : "inactive");
+  }
 }
